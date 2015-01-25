9273a8333cbc330f759ec446a4c2d45dcf87d5ed
douglas manzelmann <dougamanzelmann@gmail.com>
2015-01-25 14:13:47 -0500

    Setting up gui

--- src/SorcerersCave.java
+++ src/SorcerersCave.java
@@ -1,9 +1,23 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by test on 1/24/15.
  */
-public class SorcerersCave {
-    JPanel jpanel = new JPanel();
+public class SorcerersCave extends JFrame {
+    private JLabel jlblHello = new JLabel("Hello", JLabel.CENTER);
 
+    public static void main(String[] args) {
+        SorcerersCave frame = new SorcerersCave();
+        frame.pack();
+        frame.setTitle("Hello");
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+
+    public SorcerersCave() {
+        add(jlblHello, BorderLayout.CENTER);
+    }
 }
