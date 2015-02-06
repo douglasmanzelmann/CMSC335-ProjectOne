d1a37b4a8efb6c7cba3b8c1cf6cefafb935ac1a7
douglas <dougamanzelmann@gmail.com>
2015-02-05 22:04:38 -0500

    CaveElement abstract class

---
+++ src/CaveElement.java
@@ -0,0 +1,8 @@
+// File: Cave.java
+// Date: January 25, 2015
+// Author: Douglas Manzelmann
+// Purpose: CaveElement is an abstract class that allows for grouping
+//         and type checking of Party, Creature, Treasure and Artifact
+public abstract class CaveElement {
+
+}
